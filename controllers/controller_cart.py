24c5bb3bd6bae8b911b6134bf5e0fdbd7bbb87f8
tugrul-karacan-crea <132425381+tugrul-karacan-crea@users.noreply.github.com>
2023-06-30 17:19:46 -0700

    service_cart done

--- controllers/controller_cart.py
+++ controllers/controller_cart.py
@@ -1,10 +1,7 @@
-import jwt
+
 from flask import request, current_app
 from flask_restx import Resource, Namespace, fields
-from models.models import db
-from models.model_customer import Customer
-from models.model_cart import Cart
-from models.model_product import Product
+
 from service.service_cart import CartService
 from service.service_token import CustomerService
 
@@ -33,67 +30,26 @@ class AddToCartResource(Resource):
     @ns_cart.expect(add_cart_model)
     def post(self):
         token = request.headers.get('Authorization')
+        if token is None:
+            return {'message': 'You need to sign in to see your Cart my bruh'}, 401
+        customer_service = CustomerService()
+        customer = customer_service.validate_token(token)
+        if customer is None:
+            return {'message': 'No Customer like that!'}, 401
 
-        if not token:
-            return {'message': 'You need to sign in to buy stuff dude'}, 401
-        try:
-            decoded_token = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
-        except jwt.InvalidTokenError:
-            return {'message': 'Invalid token'}, 401
-
-        username = decoded_token.get('customer')
-        print(username)
-        customer = Customer.query.filter_by(username=username).first()
-        if not customer:
-            return {'message': 'Invalid user'}, 401
         customer_id = customer.id
 
         data = request.get_json()
         product_id = data.get('product_id')
         quantity = data.get('quantity')
 
-        if quantity == 0:
-            print("success")
+        # if quantity == 0:
+        #     print("success")
 
-        product = Product.query.filter_by(id=product_id).first()
-        if not product:
-            return {'message': 'we dont have it. come tomorrow!'}, 401
-        previous_order = Cart.query.filter_by(id=product_id).filter_by(customer_id=customer_id).first()
+        cart_service = CartService()
+        response = cart_service.add_to_cart(customer_id, product_id, quantity)
 
-        if previous_order:
-            if quantity == 0:
-                db.session.delete(previous_order)
-                db.session.commit()
-                return {
-                    'Message': 'Product deleted. Its gone.',
-                }, 201
-
-            previous_order.price = product.price * quantity
-            previous_order.quantity = quantity
-            return {
-                'Message': 'Product details updated Mr. Adams',
-                'customer_id': customer.id,
-                'Product_Id': previous_order.product_id,
-                'Product_Name': product.name,
-                'Quantity': previous_order.quantity,
-                'price': previous_order.price
-            }, 201
-
-        else:
-            price = product.price * quantity
-
-        cart = Cart(customer_id=customer_id, product_id=product_id, quantity=quantity, price=price)
-
-        db.session.add(cart)
-        db.session.commit()
-
-        return {
-            'customer_id': customer.id,
-            'Product_Id': cart.product_id,
-            'Product_Name': product.name,
-            'Quantity': cart.quantity,
-            'price': cart.price
-        }, 201
+        return response
 
     @ns_cart.route('/view_cart')
     class CartViewResource(Resource):
@@ -109,52 +65,9 @@ class AddToCartResource(Resource):
 
             customer = customer_service.validate_token(token)
             if customer is None:
-                return {'message': 'You need to sign in to see your Cart bruh'}, 401
+                return {'message': 'No Customer like that!'}, 401
 
             cart_service = CartService()
             cart_items = cart_service.get_cart_items(customer.id)
 
             return {"customer_id": customer.id, 'cart_items': cart_items}
-
-# @ns_cart.route('/view_cart')
-# class CartViewResource(Resource):
-#     @ns_cart.doc('view_cart', responses={
-#         200: 'Cart fetched successfully',
-#         401: 'Unauthorized access'
-#     })
-#     def get(self):
-#         def token_check(token):
-#             if not token:
-#                 return {'message': 'You need to sign in to see your Cart bruh'}, 401
-#             try:
-#                 decoded_token = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
-#             except jwt.InvalidTokenError:
-#                 return {'message': 'Invalid token'}, 401
-#
-#             username = decoded_token.get('customer')
-#             customer = Customer.query.filter_by(username=username).first()
-#             return customer
-#
-#
-#         customer = token_check(request.headers.get('Authorization'))
-#
-#
-#         try:
-#             customer_id = customer.id
-#         except:
-#             return customer
-#
-#         carts = Cart.query.filter_by(customer_id=customer_id).all()
-#
-#         cart_list = []
-#         for cart in carts:
-#             cart_dict = {
-#                 'name': cart.product_id,
-#                 'count': cart.quantity,
-#                 'price': cart.price,
-#             }
-#             cart_list.append(cart_dict)
-#
-#         return {"customer_id": customer_id, 'cart_items': cart_list}
-
-
--- service/service_cart.py
+++ service/service_cart.py
@@ -1,4 +1,6 @@
 from models.model_cart import Cart
+from models.model_product import Product
+from models.models import db
 
 
 class CartService:
@@ -15,3 +17,41 @@ class CartService:
             cart_list.append(cart_dict)
 
         return cart_list
+
+    def add_to_cart(self, customer_id, product_id, quantity):
+        product = Product.query.filter_by(id=product_id).first()
+        if not product:
+            return {'message': 'we dont have it. come tomorrow!'}, 401
+
+        cart = Cart.query.filter_by(product_id=product_id, customer_id=customer_id).first()
+        if cart:
+            if quantity == 0:
+                db.session.delete(cart)
+                db.session.commit()
+                return {
+                    'Message': 'Product deleted. It has been removed from your cart.',
+                }, 201
+
+            cart.price = product.price * quantity
+            cart.quantity = quantity
+            return {
+                'Message': 'Product details updated.',
+                'customer_id': customer_id,
+                'Product_Id': cart.product_id,
+                'Product_Name': product.name,
+                'Quantity': cart.quantity,
+                'price': cart.price
+            }, 201
+        else:
+            price = product.price * quantity
+            cart = Cart(customer_id=customer_id, product_id=product_id, quantity=quantity, price=price)
+            db.session.add(cart)
+            db.session.commit()
+
+            return {
+                'customer_id': customer_id,
+                'Product_Id': cart.product_id,
+                'Product_Name': product.name,
+                'Quantity': cart.quantity,
+                'price': cart.price
+            }, 201
