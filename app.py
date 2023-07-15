bdd15a7b3eafe8c795b424c80b517cb75359ac6d
Furkan Erden <48357195+FurkanErden@users.noreply.github.com>
2023-07-15 23:24:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ authorizations = {
 
 db.init_app(app)
 
-
+# Test Furkan
 
 
 api = Api(app, version='1.0', title='Noodle Store API', description='A Noodle Store API', doc='/swagger/', authorizations=authorizations, security='apikey')
